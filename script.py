c0fc9a7f24d65f903186fdd998cac4a3866f3898
davi aredes <daviaredes@gmail.com>
2022-07-21 18:19:01 -0300

    21/7/22

---
+++ script.py
@@ -0,0 +1,26 @@
+# Fazer basicamento todos os mecanismos de um calculador e depois quando tudo estiver rodando liso fazer um GUI usando o pysimplegui 
+
+# Bibliotecas ultilizadas
+import math as mt
+
+while True:
+
+    numero1 = int(input('Qual vai ser a sua primeira opçao: '))
+    print(' * Multiplicaçao\n + Adiçao\n - Subitraçao\n / Divisao\n √ Raiz Quadrada ')
+    sinais = str(input('Qual sera a sua opçao: ').lower)
+    numero2 = int(input('Seu outro numero ser: '))
+
+
+
+
+
+'''    
+    if numero1 != int:
+        break
+    if  sinais == '+' or 'adicao':
+        print(numero1 + numero2)
+    if sinais == '-' or 'subitraçao':
+        print(numero1 - numero2)
+    if sinais == '/' or 'divisao':
+        print(numero1 / numero2)
+'''
