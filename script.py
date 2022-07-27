ecfa21129e2ad320e76eeec8b091e1f75ae12898
davi aredes <daviaredes@gmail.com>
2022-07-27 19:13:37 -0300

    27/7/22

--- script.py
+++ script.py
@@ -13,33 +13,43 @@ import math as mt
 
 cont_sair = 0
 
+
 while True:
-    num1 = float(input('Primeiro Numero: '))
-    print('* Multiplicaçao\n + Adiçao \n - Subtraçao\n / Divisao\n // Raiz Quadrada')
+    
+    print('* Multiplicaçao\n+ Adiçao \n- Subtraçao\n/ Divisao\n// Raiz Quadrada')
     opera = str(input('Escolha um: '))
-    cont_sair += 1
+    
     if opera == '//':
         raiz_quadradad = int(input('Qual raiz deseja saber: '))
         raiz = mt.sqrt(raiz_quadradad)
-        print(f'A Raiz Quadrada de {raiz_quadradad} e {raiz}')
+        print(f'A Raiz Quadrada de {raiz_quadradad} e  {raiz}')
         cont_sair += 1
         continue
-    else:
-        num2 = float(input('Segundo Numero: '))
+
+    num1 = float(input('Primeiro Numero: '))
+    num2 = float(input('Segundo Numero: '))
+
+        
+    if opera == '*':
+        mult = (num1 * num2)
+        print(f' A multiplicaçao dara: {num1} * {num2} == {mult}')
+        cont_sair += 1
+    elif opera == '+' or 'adiçao':
+        adic = (num1 + num2)
+        print(f'A soma dara: {num1} + {num2} == {adic}')
+        cont_sair += 1 
+    if opera  == '-':
+        subtr =  (num1 - num2)
+        print(f'A subtraçao dara: {num1} - {num2} == {subtr}')
+        cont_sair += 1
+    elif opera == '/':
+        divi = (num1 / num2)
+        print(f'A divisao dara {num1} / {num2} ==  {divi}')
+        cont_sair += 1
     if cont_sair == 3:
         saida = str(input('Deseja sair: '))
-        if saida == 'sim' or 's':
-            print('Ate mais tchau')
+        if saida == 'sim':
             break
-    else:
-        cont_sair = 0 
-        continue
-    break
-
-if opera == '*':
-    mult = (num1 * num2)
-    print(f' A multiplicaçao dara: {num1} * {num2} == {mult}')
-elif opera == '+' or 'adiçao':
-    adic = (num1 + num2)
-    print(f'A soma dara {num1} + {num2} == {adic}')
-
+        else:
+            cont_sair = 0
+            continue
