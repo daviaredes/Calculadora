9b63029863cfad5cd74fe77842d92feb2d92ffbc
davi aredes <daviaredes@gmail.com>
2022-07-28 17:18:49 -0300

    Finalizado 1.0

--- script.py
+++ script.py
@@ -1,55 +1,57 @@
-# Fazer basicamento todos os mecanismos de um calculador e depois quando tudo estiver rodando liso fazer um GUI usando o pysimplegui 
-# falar oque cada mecanismos faz usando # obrigatorio 
-# exemplo
-# pede o primeiro numro:
-# pede qual operador sera ultilizado 
-# pede outro valor:
-# Retorna com o calculo ja feito
-    # isto em um loop
-
 # Bibliotecas ultilizadas
 import math as mt
+from time import sleep
 
+# Apenas um contador para quando o sistema der um que 2 loops (Podemos almentar ou diminuir a frequencia)
+contador_saida = 0
 
-cont_sair = 0
-
-
+# Loop
 while True:
-    
-    print('* Multiplicaçao\n+ Adiçao \n- Subtraçao\n/ Divisao\n// Raiz Quadrada')
-    opera = str(input('Escolha um: '))
-    
-    if opera == '//':
-        raiz_quadradad = int(input('Qual raiz deseja saber: '))
-        raiz = mt.sqrt(raiz_quadradad)
-        print(f'A Raiz Quadrada de {raiz_quadradad} e  {raiz}')
-        cont_sair += 1
-        continue
+    try:
+        # Painel de operçoes possiveis
+        print('* Multiplicaçao\n+ Adiçao \n- Subtraçao\n/ Divisao\n// Raiz Quadrada')
+        operaçoes = str(input('Escolha um: '))
 
-    num1 = float(input('Primeiro Numero: '))
-    num2 = float(input('Segundo Numero: '))
-
-        
-    if opera == '*':
-        mult = (num1 * num2)
-        print(f' A multiplicaçao dara: {num1} * {num2} == {mult}')
-        cont_sair += 1
-    elif opera == '+' or 'adiçao':
-        adic = (num1 + num2)
-        print(f'A soma dara: {num1} + {num2} == {adic}')
-        cont_sair += 1 
-    if opera  == '-':
-        subtr =  (num1 - num2)
-        print(f'A subtraçao dara: {num1} - {num2} == {subtr}')
-        cont_sair += 1
-    elif opera == '/':
-        divi = (num1 / num2)
-        print(f'A divisao dara {num1} / {num2} ==  {divi}')
-        cont_sair += 1
-    if cont_sair == 3:
-        saida = str(input('Deseja sair: '))
-        if saida == 'sim':
-            break
-        else:
-            cont_sair = 0
+        if operaçoes == '//': # Raiz Quadrada
+            raiz_quadradad = float(input('Qual raiz deseja saber: '))
+            raiz = mt.sqrt(raiz_quadradad)
+            print(f'A Raiz Quadrada de {raiz_quadradad} e  {raiz}')
+            contador_saida += 1
             continue
+        
+        # Si nao for uma Raiz Quadrada ele pede dois numero para calcular
+        num1 = float(input('Primeiro Numero: '))
+        num2 = float(input('Segundo Numero: '))
+    # Caso aja algum erro 
+    except: 
+        print('ERRO, Algo esta errado')
+        contador_saida = 0
+        sleep(1)
+        continue
+    else:
+
+        if operaçoes == '*': # Multiplicaçao
+            mult = (num1 * num2)
+            print(f' A multiplicaçao dara: {num1} * {num2} == {mult}')
+            contador_saida += 1
+        elif operaçoes == '+' : # Adiçao
+            adic = (num1 + num2)
+            print(f' A soma dara: {num1} + {num2} == {adic}')
+            contador_saida += 1 
+        elif operaçoes  == '-': # Subtraçao
+            subtr =  (num1 - num2)
+            print(f' A subtraçao dara: {num1} - {num2} == {subtr}')
+            contador_saida += 1
+        elif operaçoes == '/': # Divisao
+            divi = (num1 / num2)
+            print(f' A divisao dara {num1} / {num2} ==  {divi}')
+            contador_saida += 1
+
+        if contador_saida == 2: # Sistema de saida 
+            saida = str(input('Deseja sair: ')[0])
+            if saida == 's':
+                break
+            else:
+                contador_saida = 0
+                continue
+
