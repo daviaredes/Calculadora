eed64b3500d8aeddc9c39558328166fcdd390ff4
Davi Aredes <daviaredes@gmail.com>
2022-12-16 15:37:20 -0300

    new format, day 2

--- calculadora.py
+++ calculadora.py
@@ -1,69 +1,56 @@
-from PySimpleGUI import PySimpleGUI as sg
-from random import randint
+from PySimpleGUI import *
 
+theme('DarkGrey14')
 
-def start_window():
-    sg.theme('DarkGrey14')
-    
-    layout = [
-        [
-            sg.Text('' * 10),
-            sg.Text('', k='tela', s=(5, 2)),
-            sg.Text('' * 10)
-        ],
-        [
-            sg.Button('//', k='potencia', s=(5, 2)),
-            sg.Button('%', k='porcetagem', s=(5, 2)),
-            sg.Button('C', k='clear', s=(5, 2)),
-            sg.Button('<X', k='delet', s=(5, 2))
-        ],
-        [
-            sg.Button('1', k='um' , s=(5, 2)), 
-            sg.Button('2', k='dois', s=(5, 2)),
-            sg.Button('3', k='tres', s=(5, 2)),
-            sg.Button('+', k= 'mais', s=(5, 2))
-        ],
-        [
-            sg.Button('4', k='quatro', s=(5, 2)),
-            sg.Button('5', k='cinco', s=(5, 2)),
-            sg.Button('6', k='seis', s=(5, 2)),
-            sg.Button('- ', k='menos', s=(5, 2))
-        ],
-        [
-            sg.Button('7', k='sete', s=(5, 2)),
-            sg.Button('8', k='oito', s=(5, 2)),
-            sg.Button('9', k='nove', s=(5, 2)),
-            sg.Button('*', k='vezes', s=(5, 2))
-        ],
-        [
-            sg.Button('/ ', k='divisao', s=(5, 2)),
-            sg.ReadFormButton('0', k='zero', s=(5, 2)),
-            sg.Button(',', k='virgula', s=(5, 2)),
-            sg.ReadFormButton('=', k='igual', s=(5, 2))
-        ]
+layout = [
+    [
+        Text('', key='tela')
+    ],
+    [
+        ReadFormButton('C'),
+        ReadFormButton('<<')
+    ],
+    [
+        ReadFormButton('1'),
+        ReadFormButton('2'),
+        ReadFormButton('3'),
+        ReadFormButton('+')
+    ],
+    [
+        ReadFormButton('4'),
+        ReadFormButton('5'),
+        ReadFormButton('6'),
+        ReadFormButton('-')
+    ],
+    [
+        ReadFormButton('7'),
+        ReadFormButton('8'),
+        ReadFormButton('9'),
+        ReadFormButton('*')
+    ],
+    [
+        ReadFormButton('/'),
+        ReadFormButton('0'),
+        ReadFormButton(','),
+        ReadFormButton('=')
     ]
-    return sg.Window('start', layout=layout, finalize=True, resizable=True)
+]
 
-window1 = start_window()
+Wind = FlexForm('Calculadora', default_button_element_size=(5, 2))
+Wind.Layout(layout) 
 
 Result = ''
 
-def operaçoes():
-    pass
-
 while True:
-    window, event, values = sg.read_all_windows()
 
-    if window == window1 and event == sg.WINDOW_CLOSED:
-        break
+    button, values = Wind.Read()
 
-    if window == window1 and event == 'clear':
+    if button == 'C':
         Result = ''
-        window.find_element('tela').update(Result)
-        print('test botao clear')
-
-    elif window == window1 and event == 'delet':
-        Result == [-1]
-        window.find_element('tela').update(Result)
-        print('test botao delet')
-
+        Wind.find_element('tela').update(Result)
+    elif button == '<<':
+        Result = Result[:-1]
+        Wind.find_element('tela').update(Result)
+        
+    if button == 'Quit' or button == None:
+        break
